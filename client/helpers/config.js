49a28ea2ba9fac2e7a9b4d9b6e772648c3649e4f
sgrauel <shawn.m.grauel@gmail.com>
2017-05-11 22:21:19 -0400

    accounts-ui and accounts modules integrated

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -6,12 +6,5 @@ Accounts.config({
 });
 
 Accounts.ui.config({
-        passwordSignupFields: 'EMAIL_ONLY',
-        loginPath: '/login',
-        signUpPath: '/signup',
-        resetPasswordPath: '/reset-password',
-        profilePath: '/profile',
-        onSignedInHook: () => Router.go('/Home'),
-    onSignedOutHook: () => Router.go('/Home'),
-    minimumPasswordLength: 6
+        passwordSignupFields: 'EMAIL_ONLY'
 });
