dfb693f1e540756f8aae96fefedf70c9099b1d4b
Shawn Grauel <shawn.m.grauel@gmail.com>
2018-08-08 21:47:43 -0400

    added tracking for signups/sign-ins

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,3 +1,77 @@
+/*
+Tracker.autorun(function() {
+
+    console.log('logged with uid:' + (Meteor.userId() && Meteor.loggingIn()));
+    console.log(Meteor.userId());
+    console.log(Meteor.loggingIn());
+    if (Meteor.userId() && Meteor.loggingIn()) {
+
+      ga('set', 'page', '/login');
+
+      let userId = Meteor.userId();
+      let user = Meteor.user();
+
+      console.log('hasNotLoginCount: ' + !Meteor.user().hasOwnProperty('login_count'));
+      if (!Meteor.user().hasOwnProperty('login_count')) {
+        user.login_count = 1;
+        Meteor.users.update({_id: userId},user);
+      } else {
+        const inc = user.login_count + 1;
+        Meteor.users.update({_id: userId},{$set: {login_count: inc }});
+      }
+
+      user = Meteor.user();
+
+      console.log('user login count: ' + user.login_count);
+      if (user.login_count > 1) {
+        ga('send', 'pageview', {
+          'dimension1' : 'false'
+        });
+      } else {
+        ga('send', 'pageview', {
+          'dimension1' : 'true'
+        });
+      }
+
+    }
+
+});
+*/
+
+Accounts.onLogin(function(){
+
+  ga('set', 'page', '/login');
+
+  let userId = Meteor.userId();
+  let user = Meteor.user();
+
+  console.log('hasNotLoginCount: ' + !Meteor.user().profile.hasOwnProperty('login_count'));
+  if (!Meteor.user().profile.hasOwnProperty('login_count')) {
+    Meteor.users.update({"_id": userId},{$set: {"profile.login_count": 1 }});
+    // Meteor.users.update({_id: userId, login_count: {$exists: false}},{$set: {login_count: 1 }});
+
+  } else {
+    const inc = user.profile.login_count + 1;
+    // Meteor.users.update({_id: userId},{$set: {login_count: inc }});
+    Meteor.users.update({"_id": userId},{$set: {"profile.login_count": inc }});
+  }
+
+  user = Meteor.user();
+
+  console.log('user login count: ' + user.profile.login_count);
+  if (user.profile.login_count > 1) {
+    ga('send', 'pageview', {
+      'dimension1' : 'false'
+    });
+  } else {
+    ga('send', 'pageview', {
+      'dimension1' : 'true'
+    });
+  }
+
+});
+
+
 Template.Home.events({
    "click #bookingButton": function() {
        var user = Meteor.users.findOne();
